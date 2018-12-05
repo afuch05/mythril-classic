de562335a62703429e085de7088a3e543f84e94f
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2018-12-05 16:40:43 +0100

    update of ownership detection and printout

--- mythril/analysis/modules/ownership.py
+++ mythril/analysis/modules/ownership.py
@@ -3,64 +3,62 @@ import logging
 from mythril.analysis.report import Issue
 
 
-
 class OwnerCheck:
-    def __init__(self, is_static=False, is_storage_word=False, constraint=None, caller_value=None, comparison=None):
-        self.is_static = is_static
-        self.is_storage_word = is_storage_word,
+    def __init__(self, is_owner_check=False, owner_comparison=None, constraint=None):
+        self.is_owner_check = is_owner_check
+        self.owner_comparison = owner_comparison
         self.constraint = constraint
-        self.caller_value = caller_value
-        self.comparison = comparison
+
+
+class OwnershipFunction:
+    def __init__(self, function_name="", static_address=None, storage_address=None, owner_comparison=None, constraint=None):
+        self.function_name = function_name
+        self.static_address = static_address
+        self.storage_address = storage_address
+        self.owner_comparison = owner_comparison
+        self.constraint = constraint
 
 
 def execute(statespace):
     logging.debug("Executing module: OWNERSHIP")
 
-    issues = []
+    ''' list with ownership functions '''
+    ownership_functions = []
 
-    ''' dictionary: key is the contract-name, value is a list of function names with ownership pattern '''
-    ownership_contract_to_functions = dict()
-    ownership_function_to_constraint = dict()
+    '''
+    function_names_all = set()
+    function_names_ownership = set()
+    '''
 
     for k in statespace.nodes:
         node = statespace.nodes[k]
-
-        ownership_contract_to_functions.setdefault(node.contract_name, [])
-
+        '''function_names_all.add(node.function_name)'''
         if len(node.constraints) > 0:
             last_constraint = node.constraints[-1]
+
             owner_check = is_caller_check(last_constraint)
+            if owner_check.is_owner_check:
+                '''function_names_ownership.add(node.function_name)'''
 
-            if owner_check is not None:
-                ownership_contract_to_functions[node.contract_name].append(node.function_name)
-                ownership_function_to_constraint[node.function_name] = owner_check.constraint
-
-                '''
-                print("contract: " + node.contract_name)
-                print("function: " + node.function_name)
-                print("caller value: " + str(owner_check.caller_value))
-                print("caller comparison: " + str(owner_check.comparison))
-                print("is storage word comparison: " + str(owner_check.is_storage_word))
-                print("constraint: " + str(owner_check.constraint))
-
-                issues.append(
-                    Issue(
-                        contract=node.contract_name,
-                        function_name=node.function_name,
-                        address=None,
-                        swc_id=None,
-                        title="Ownership",
-                        _type="Ownership",
-                        description="Found ownership function",
-                        bytecode=node.states[-1].environment.code.bytecode,
-                        debug=str(owner_check),
+                ownership_functions.append(
+                    OwnershipFunction(
+                        node.function_name,
+                        get_static_address(owner_check.owner_comparison),
+                        get_storage_address(owner_check.owner_comparison),
+                        owner_check.owner_comparison,
+                        owner_check.constraint,
                     )
                 )
-                '''
-    print("Ownership functions: " + str(ownership_contract_to_functions))
-    print("Function ownership constraint: " + str(ownership_function_to_constraint))
 
-    return issues
+    for o in ownership_functions:
+        print(o.function_name + ";" + o.static_address + ";" + o.storage_address + ";" + str(o.owner_comparison) + ";" + str(o.constraint))
+
+    '''
+    for fn in function_names_all.difference(function_names_ownership):
+        print(fn + "; " + str(False))
+    '''
+
+    return []
 
 
 def is_caller_check(constraint) -> OwnerCheck:
@@ -70,7 +68,7 @@ def is_caller_check(constraint) -> OwnerCheck:
     then, check if its argument is of type x op y
     """
     if constraint is None:
-        return None
+        return OwnerCheck(False)
 
     if str(constraint).startswith("Not"):
         arg = constraint.arg(0)
@@ -83,13 +81,13 @@ def is_caller_check(constraint) -> OwnerCheck:
                 rhs_is_caller = is_caller_argument(arg.arg(1))
 
                 if lhs_is_caller and not rhs_is_caller:
-                    return OwnerCheck(False, is_storage_argument(arg.arg(1)), constraint, arg.arg(0), arg.arg(1))
+                    return OwnerCheck(True, arg.arg(1), constraint)
                 elif rhs_is_caller and not lhs_is_caller:
-                    return OwnerCheck(False, is_storage_argument(arg.arg(0)), constraint, arg.arg(1), arg.arg(0))
+                    return OwnerCheck(True, arg.arg(0), constraint)
 
     ''' TODO: check if constraint is  x != y '''
 
-    return None
+    return OwnerCheck(False)
 
 
 def is_caller_argument(arg) -> bool:
@@ -98,3 +96,17 @@ def is_caller_argument(arg) -> bool:
 
 def is_storage_argument(arg) -> bool:
     return "storage" in str(arg)
+
+
+def get_static_address(arg) -> str:
+    strarg = str(arg)
+    if "storage" not in strarg:
+        return strarg
+    return ""
+
+
+def get_storage_address(arg) -> str:
+    strarg = str(arg)
+    if "storage" in strarg:
+        return strarg
+    return ""
